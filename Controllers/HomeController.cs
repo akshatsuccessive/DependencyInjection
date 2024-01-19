3e891241d104d550ac974fb17f1cedd1fc4422b4
akshat.kumar <akshat.kumar@successive.tech>
2024-01-19 11:11:53 +0530

    Adding services to program.cs and adding controllers

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyDependencyInjection.Models;
+
+namespace MyDependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        public JsonResult Index()
+        {
+            StudentRepository repository = new StudentRepository();
+            List<Student> allStudentDetails = repository.GetAllStudent();
+            return Json(allStudentDetails);
+        }
+        //[Route("{Id}")]
+        public JsonResult GetStudentDetails(int Id)      // https://localhost:7041/Home/GetStudentDetails/1
+        {
+            StudentRepository repository = new StudentRepository();
+            Student studentDetails = repository.GetStudentById(Id);
+            return Json(studentDetails);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add MVC services to the container
+builder.Services.AddMvc();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+app.UseRouting();
+app.MapControllers();   
+/*
+app.UseEndpoints(endpoints =>
+{
+    //Configuring the MVC middleware to the request processing pipeline
+    endpoints.MapDefaultControllerRoute();
+});
+*/
+
 
 app.Run();
 
