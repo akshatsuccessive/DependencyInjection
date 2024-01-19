bdaf99874977a485302dbf3abb5bec4fc41cb0c3
akshat.kumar <akshat.kumar@successive.tech>
2024-01-19 12:17:17 +0530

    Adding services by AddSIngleton Method in program.cs

--- Program.cs
+++ Program.cs
@@ -1,22 +1,38 @@
+using MyDependencyInjection.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add MVC services to the container
+// (i) Add MVC services to the container
 builder.Services.AddMvc();
+
+// Add Application Service to the Container
+
+builder.Services.AddSingleton<IStudentRepository, StudentRepository>();
+//builder.Services.AddSingleton(typeof(IStudentRepository), typeof(StudentRepository));
+//builder.Services.AddTransient<IStudentRepository, StudentRepository>();
+//builder.Services.AddTransient(typeof(IStudentRepository), typeof(StudentRepository));
+//builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+//builder.Services.AddScoped(typeof(IStudentRepository), typeof(StudentRepository));
+
 
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 
 app.UseRouting();
-app.MapControllers();   
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+
 /*
 app.UseEndpoints(endpoints =>
 {
-    //Configuring the MVC middleware to the request processing pipeline
+    // (ii) Configuring the MVC middleware to the request processing pipeline
     endpoints.MapDefaultControllerRoute();
 });
 */
-
 
 app.Run();
 
