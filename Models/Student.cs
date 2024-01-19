264364ea1385b6f723ed24d0eb9b3c7d905efb18
akshat.kumar <akshat.kumar@successive.tech>
2024-01-19 10:58:27 +0530

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyDependencyInjection.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string? Name { get; set; }
+        public string? Branch { get; set; }
+        public int Age { get; set; }   
+    }
+}
---
+++ Models/StudentRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace MyDependencyInjection.Models
+{
+    public class StudentRepository : IStudentRepository
+    {
+        public List<Student> StudentsData()
+        {
+            return new List<Student>()
+            {
+                new Student() {StudentId  = 1, Name = "Akshat", Branch = "IT", Age = 21},
+                new Student() {StudentId  = 2, Name = "Yash", Branch = "CSE", Age = 20},
+                new Student() {StudentId  = 3, Name = "Bohra", Branch = "MCA", Age = 23},
+                new Student() {StudentId  = 4, Name = "Govind", Branch = "SE", Age = 22},
+                new Student() {StudentId  = 5, Name = "Prince", Branch = "ECE", Age = 24}
+            };
+        }
+        public List<Student> GetAllStudent()
+        {
+            return StudentsData();
+        }
+
+        public Student GetStudentById(int StudentId)
+        {
+            return StudentsData().FirstOrDefault(s => s.StudentId == StudentId) ?? new Student();
+        }
+    }
+}
